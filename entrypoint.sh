ab152d2c4e02f17e3db390a35e2a5a688d7ba6db
Oliver Butler <dev@oliverbutler.uk>
2020-10-25 20:10:53 +0000

    Intial

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ! -x /home/minecraft/enigmatica2/ServerStartLinux.sh ]]; then
+  cp -r /tmp/enigmatica2/* .
+fi
+
+## Config via Environment Variables
+sed -i "/motd\s*=.*/ c motd=$MOTD" /home/minecraft/enigmatica2/server.properties
+sed -i "/level-name\s*=.*/ c level-name=$MAP" /home/minecraft/enigmatica2/server.properties
+sed -i "/difficulty\s*=.*/ c difficulty=$DIFFICULTY" /home/minecraft/enigmatica2/server.properties
+sed -i "/max-players\s*=.*/ c max-players=$MAXPLAYERS" /home/minecraft/enigmatica2/server.properties
+sed -i "/pvp\s*=.*/ c pvp=$PVP" /home/minecraft/enigmatica2/server.properties
+sed -i "/view-distance\s*=.*/ c view-distance=$VIEWDISTANCE" /home/minecraft/enigmatica2/server.properties
+
+## Launch Server
+./ServerStartLinux.sh
